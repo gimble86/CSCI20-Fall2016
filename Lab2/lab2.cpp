71fcc12c34e3da8d7b8917a42f540bfb4d4359aa
Frank Ryan Gimble <fgimble001@student.butte.edu>
2016-08-26 04:15:52 +0000

    Lab 2 Submission

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // computer randomly selects a number between 1 and 100 and stores it as a variable
+    // Set points to zero initially then keep a running total
+    // Display Points initially and after each user input
+    // computer asks user to guess a number between 1 and 100
+    // user inputs a number
+    // if the number is to low, computer tells user to enter a higher number and subtracts 1 point from score
+    // if the number is to high, computer tells user to enter a lower number and subtracts 1 point from score
+    // if the number is correct, computer tells user they guessed the right number and gives the user 10 points
+    // Game runs until user quits
+    
+}
