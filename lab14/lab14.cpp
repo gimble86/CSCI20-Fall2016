f64d7b96dca2c33c8d069094130878be1986cf61
Frank Ryan Gimble <fgimble001@student.butte.edu>
2016-10-18 18:40:23 +0000

    lab 14 submission

---
+++ lab14/lab14.cpp
@@ -0,0 +1,50 @@
+//Ryan Gimble 10-18-16
+//Debugged this pre-written program and produced output outlining the process
+
+//Grade Average
+// Read in a series of grades, terminated by a -1.
+// Then output the average and minimum of those grades.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+// Declare variables
+    int count = 0;
+    double total = 0;
+    int minimum = 1000;
+    int grade = 0;
+    int userInput = 0;
+
+    //Read numbers in until we see the -1
+    //Sum up grades as we do, and look for minimum
+    while (userInput != -1) {
+        cin >> userInput;
+        
+        if (userInput >= 0){
+        grade = userInput;
+        total = total + grade;
+        count = count + 1;
+        }
+        
+        //fixme
+        if (grade < minimum) {
+        minimum = grade;
+        }
+    }
+    
+    // Output results
+    double average = total / count;
+    cout << "Average was " << average << endl;
+    cout << "Minimum was " << minimum << endl;
+    return 0;
+    
+    //instructional comments need to be commented out
+    //ran compiler, looked for syntax errors.
+    //read through code looking for obvious logic errors.  
+    //if (grade < minimum) looks like a problem because it grade is initialized to zero.  initiallized minimum to 1000 instead since that should be out of range of any grading scale. 
+    //entering negative 1 into grade causes total to be wrong
+    //created new variable and if statement in loop to correct problem created by entering -1 into grade
+    
+}
